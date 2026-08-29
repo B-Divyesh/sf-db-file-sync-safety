76838ffee65cb46d30ad898bd96b234a61ddb0f1
Param Factory <factory@sociobot.in>
2026-08-29 05:23:32 +0000

    chore: prepare v0.1.3 release

--- packaging/homebrew/dbsync-safe.rb
+++ packaging/homebrew/dbsync-safe.rb
@@ -1,20 +1,20 @@
 class DbsyncSafe < Formula
   desc "Make verified SQLite snapshots before file sync"
   homepage "https://db-file-sync-safety.sociobot.in"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/B-Divyesh/sf-db-file-sync-safety/releases/download/v0.1.2/dbsync-safe-macos-aarch64.tar.gz"
+      url "https://github.com/B-Divyesh/sf-db-file-sync-safety/releases/download/v0.1.3/dbsync-safe-macos-aarch64.tar.gz"
       sha256 "54b4ac4478d00568e65487c303e74a8838d0738bc09494c5c0c239e282075108"
     else
-      url "https://github.com/B-Divyesh/sf-db-file-sync-safety/releases/download/v0.1.2/dbsync-safe-macos-x86_64.tar.gz"
+      url "https://github.com/B-Divyesh/sf-db-file-sync-safety/releases/download/v0.1.3/dbsync-safe-macos-x86_64.tar.gz"
       sha256 "9c5febe8573c78a695b8095430f4b7cb7fc88af08e11a98fe6d45ce55cf2387a"
     end
   end
 
   on_linux do
-    url "https://github.com/B-Divyesh/sf-db-file-sync-safety/releases/download/v0.1.2/dbsync-safe-linux-x86_64.tar.gz"
+    url "https://github.com/B-Divyesh/sf-db-file-sync-safety/releases/download/v0.1.3/dbsync-safe-linux-x86_64.tar.gz"
     sha256 "af8b4a7627a6b69dcc123524cdb330fcd4e37841e45c034757011bc7534625e7"
   end
 
